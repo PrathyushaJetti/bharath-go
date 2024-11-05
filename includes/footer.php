5b62acaa61271ead9d4da5d6878a83a86c03a19d
unknown <srikanth8605@gmail.com>
2024-11-05 12:25:41 +0530

    added header and footer

---
+++ includes/footer.php
@@ -0,0 +1,130 @@
+<footer class="footer-section">
+        <div class="footer-top-wrap bg-grey">
+            <div class="container">
+                <div class="footer-top">
+                    <div class="row align-items-end">
+                        <div class="col-lg-8">
+                            <div class="brand">
+                                <a class="footer-logo" href="index.html"><img src="assets/images/bharath-go.png"
+                                        alt="logo"></a>
+                                <p>Fast, affordable, and reliable rides for everyone, anytime and anywhere you need to go.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="footer-call">
+                                <i class="las la-phone-volume"></i>
+                                <p><span>Call For Taxi</span><a href="tel:6303548105"> 6303548105</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/.footer-top-wrap-->
+        <div class="footer-mid-wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item">
+                            <div class="widget-title">
+                                <h3>Quick Links </h3>
+                            </div>
+                            <ul class="footer-links">
+                                <li><a href="index.html"> FAQ'S </a></li>
+                                <li><a href="about-us.html">  privacy policy </a></li>
+                                <li><a href="our-services.html">Terms and Conditions </a></li>
+                                <li><a href="safety.html">Disclaimer </a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item footer-list">
+                            <div class="widget-title">
+                                <h3>Usefull Links</h3>
+                            </div>
+                            <ul class="footer-links">
+                                <li><a href="index.html"> Home </a></li>
+                                <li><a href="about-us.html">About Us </a></li>
+                                <li><a href="our-services.html">Services </a></li>
+                                <li><a href="safety.html">Safety </a></li>
+                                <li><a href="career.html">Careers </a></li>
+                                <li><a href="blog.html"> Blog </a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item">
+                            <div class="widget-title">
+                                <h3>Head Office</h3>
+                            </div>
+                            <ul class="footer-contact">
+                                <li><span>Location:</span>Ground Floor, Amrutha Ville, Opposite Yashoda Hospital Road, Raj Bhavan Rd, Somajiguda, Hyderabad, Telangana 500082</li>
+                                <li><span>Join Us:</span>Info@YourGmail24.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 sm-padding">
+                        <div class="footer-item subscribe-wrap">
+                            <div class="widget-title">
+                                <h3>Newsletter Signup</h3>
+                            </div>
+                            <form action="#" class="subscribe-form">
+                                <input class="form-control" type="email" name="email" placeholder="Your Email"
+                                    required="">
+                                <input type="hidden" name="action" value="mailchimpsubscribe">
+                                <button class="submit">Subscribe Now</button>
+                                <div class="clearfix"></div>
+                                <div id="subscribe-result">
+                                    <div class="subscription-success"></div>
+                                    <div class="subscription-error"></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="running-taxi">
+                <div class="taxi"></div>
+                <div class="taxi-2"></div>
+                <div class="taxi-3"></div>
+            </div>
+        </div>
+        <!--/.footer-mid-wrap-->
+        <div class="copyright-wrap">
+            <div class="container">
+                <p>© <span>2022</span> Bharath Go All Rights Reserved</p>
+            </div>
+        </div>
+        <!--/.copyright-wrap-->
+    </footer>
+    <!--/.footer-section-->
+
+    <div id="scrollup">
+        <button id="scroll-top" class="scroll-to-top">
+            <i class="las la-arrow-up"></i>
+        </button>
+    </div>
+    <!--scrollup-->
+
+    <div class="dl-cursor">
+        <div class="cursor-icon-holder"><i class="las la-times"></i></div>
+    </div>
+    <!--/.dl-cursor-->
+
+    <!--jQuery Lib-->
+    <script src="assets/js/vendor/jquary-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script src="assets/js/vendor/jquery.ajaxchimp.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/vendor/swiper.min.js"></script>
+    <script src="assets/js/vendor/jquery.datetimepicker.full.js"></script>
+    <script src="assets/js/vendor/jquery.nice-select.min.js"></script>
+    <script src="assets/js/vendor/venobox.min.js"></script>
+    <script src="assets/js/vendor/smooth-scroll.js"></script>
+    <script src="assets/js/vendor/wow.min.js"></script>
+    <script src="assets/js/book-ride.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
---
+++ includes/header.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+
+<html class="no-js" lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Ridek Online Taxi Booking HTML5 Template">
+    <meta name="author" content="DynamicLayers">
+
+    <title>iuasdiusdah</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/bharath-go.png">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/keyframe-animation.css">
+    <link rel="stylesheet" href="assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/venobox.min.css">
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/elements.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/slider.css">
+    <link rel="stylesheet" href="assets/css/blog.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+
+<body>
+
+
+    <div class="site-preloader">
+        <div class="car">
+            <div class="strike"></div>
+            <div class="strike strike2"></div>
+            <div class="strike strike3"></div>
+            <div class="strike strike4"></div>
+            <div class="strike strike5"></div>
+            <div class="car-detail spoiler"></div>
+            <div class="car-detail back"></div>
+            <div class="car-detail center"></div>
+            <div class="car-detail center1"></div>
+            <div class="car-detail front"></div>
+            <div class="car-detail wheel"></div>
+            <div class="car-detail wheel wheel2"></div>
+        </div>
+    </div><!--/.site-preloader-->
+
+    <header class="main-header">
+        <div class="top-header">
+            <div class="container">
+                <div class="top-header-wrap">
+                    <div class="top-left">
+                        <p>Reliable Taxi Service & Transport Solutions!</p>
+                    </div>
+                    <div class="top-right">
+                        <ul class="top-header-nav">
+                            <li><a href="faqs.html">Help</a></li>
+                            <li><a href="contact.html">Support</a></li>
+                            <li><a href="faqs.html">FAQ</a></li>
+                        </ul>
+                        <ul class="header-social-share">
+                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div><!--/.top-header-->
+        <div class="mid-header">
+            <div class="container">
+                <div class="mid-header-wrap">
+                    <div class="site-logo">
+                        <a href="index.html"><img src="assets/images/bharath-go.png" alt="Logo"></a>
+                    </div><!--/.site-logo-->
+                    <ul class="header-info">
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-phone-volume"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h3><span>Call us now</span><a href="tel:6303548105"> 6303548105</a></h3>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-envelope-open"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h3><span>Email now</span>Info.ridek@mail.com</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="header-info-icon">
+                                <i class="las la-map-marked-alt"></i>
+                            </div>
+                            <div class="header-info-text">
+                                <h6>Ground Floor, Amrutha Ville, Opposite Yashoda Hospital Road, Raj Bhavan Rd, Somajiguda, Hyderabad, Telangana 500082</h6>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div><!--/.mid-header-->
+        <div class="nav-menu-wrapper">
+            <div class="container">
+                <div class="nav-menu-inner">
+                    <div class="site-logo">
+                        <a href="index.html"><img src="assets/images/bharath-go.png" alt="Logo"></a>
+                    </div><!--/.site-logo-->
+                    <div class="header-menu-wrap">
+                        <ul class="nav-menu">
+                            <li class="active dropdown_menu">
+                                <a href="index.html">Home</a>
+                                
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="about-us.html">About Us</a>
+                                
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="our-services.html"> Services </a>
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="safety.html">
+                                    Safety</a>
+ 
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="career.html">Careers</a>
+                                
+                            </li>
+                            <li class="dropdown_menu">
+                                <a href="blog-grid.html">Blog</a>
+                                
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="menu-right-item">
+                        <!-- <div class="search-icon dl-search-icon">
+                            <i class="las la-search"></i>
+                        </div> -->
+                        <div class="sidebox-icon dl-sidebox-icon">
+                            <i class="las la-bars"></i>
+                        </div>
+                        <a href="book-taxi.html" class="menu-btn">Book a Taxi</a>
+                    </div>
+                    <div class="mobile-menu-icon">
+                        <div class="burger-menu">
+                            <div class="line-menu line-half first-line"></div>
+                            <div class="line-menu"></div>
+                            <div class="line-menu line-half last-line"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/.nav-menu-->
+    </header>
+    <!--/.main-header-->
